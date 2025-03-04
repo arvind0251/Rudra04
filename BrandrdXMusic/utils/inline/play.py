552fb1fe4f9a3eff3ed12cf8f9ed19c48592bf57
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-03-04 16:16:02 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -68,7 +68,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/RU_DRA_65"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Il_taj_l"
             ),
             InlineKeyboardButton(
                 text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/RU_DRA_098"
@@ -245,7 +245,7 @@ def stream_markup2(_, chat_id):
         ],
 [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/RU_DRA_65"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/Il_taj_l"
             ),
             InlineKeyboardButton(
                 text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/RU_DRA_098"
